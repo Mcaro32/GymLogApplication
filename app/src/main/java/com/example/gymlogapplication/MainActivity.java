6beb89fe388c13778a810713384e0f60c7948cdd
Manuel Caro <mcaro@csumb.edu>
2025-11-30 12:32:43 -0800

    End of video 6: updated display, cleaned toString, and prevented empty logs

--- app/src/main/java/com/example/gymlogapplication/MainActivity.java
+++ app/src/main/java/com/example/gymlogapplication/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.gymlogapplication.database.GymLogRepository;
 import com.example.gymlogapplication.database.entities.GymLog;
 import com.example.gymlogapplication.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
-
+        updateDisplay();
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,16 +51,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void insertGymLogRecord(){
+    private void insertGymLogRecord() {
+        if (mExercise.isEmpty()) {
+            return;
+        }
         GymLog log = new GymLog(mExercise, mWeight, mReps);
         repository.insertGymLog(log);
     }
+
     private void updateDisplay() {
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        Log.d(TAG, "Current Info: " + currentInfo);
-        String newDisplay = String.format(Locale.US, "Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s ", mExercise, mWeight, mReps, currentInfo);
-        binding.logDisplayTextView.setText(newDisplay);
-        Log.i(TAG, repository.getAllLogs().toString());
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if (allLogs.isEmpty()) {
+            binding.logDisplayTextView.setText("Nothing to show, time to hit the gym!");
+        }
+        StringBuilder sb = new StringBuilder();
+        for (GymLog log : allLogs) {
+            sb.append(log);
+        }
+
+        binding.logDisplayTextView.setText(sb.toString());
     }
 
     private void getInformationFromDisplay() {
